b1c3208dff5c9670a3073ec33ba23c53000d89e1
Mike <micah41224@gmail.com>
2021-07-28 09:52:58 -0400

    Began work on base structure of rows/columns to hold the time/entry/save buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/*
 var dayArray = [
     {
         id:"0",
@@ -73,30 +74,66 @@ var dayArray = [
     },
 ]
 
+*/
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 
-function listCreate() {
+var hours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+var military = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-}
 
 var rootDiv = $('#root');
 
+
 for (var i = 0; i < dayArray.length; i++) {
     // Create a new `<div>` for each ability and its text content
-    var baseRow = $('<div>');
-    
+    var baseRow = $("<div class='row' id='baseRow>");
+    var timeBox = $("<div>");
   
-    // abilityEl.textContent = abilities[i];
+    timeBox.text(dayArray.hour[i]);
     //abilityEl.text(abilities[i]);
   
     // `my-3` class adds margin on top and bottom
-    baseRow.addClass('row');
+    //baseRow.addClass('row');
+    //timeBox.addClass('hour');
   
     // Alternatively
     // var abilityEl = $("<div>" + abilities[i] + "</div>");
   
     // Add this new `<div>` to the abilities `<div>` container element.
     rootDiv.append(baseRow);
+    rootDiv.append(timeBox);
   }
+
+
+/*
+  for (var i = 0; i < abilities.length; i++) {
+    // Create a new `<div>` for each ability and its text content
+    var abilityEl = $('<li>');
+  
+    // abilityEl.textContent = abilities[i];
+    abilityEl.text(abilities[i]);
+  
+    // `my-3` class adds margin on top and bottom
+    abilityEl.addClass('my-3');
+  
+    // Alternatively
+    // var abilityEl = $("<div>" + abilities[i] + "</div>");
+  
+    // Add this new `<div>` to the abilities `<div>` container element.
+    rootEl.append(abilityEl);
+  }
+  
+  // select all `<div>` elements on the page
+  // document.querySelectorAll('div');
+  console.log($('div'));
+  
+  // Alternatively use the `$.each` method
+  // $.each(abilities, function(i, ability) {
+  //    abilityEl.append("<div>" + ability + "</div>");
+  // })
+  */
+
+
+  // Json might can be used to enable you to use the array as it can pack and unpack them in relation to local storage?
