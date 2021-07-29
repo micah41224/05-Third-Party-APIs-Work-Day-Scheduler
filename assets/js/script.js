4b85b9294ca16c370812a5a5be4bab2167965905
Mike <micah41224@gmail.com>
2021-07-29 17:40:00 -0400

    Attempting different ways of saving to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,8 @@ function timeColorShift() {
       else if(military[i] = presentTime) {
           inputForm.addClass("present");
       }
-console.log("military time " +military[i]);
-console.log("present time " +presentTime);
+//console.log("military time " +military[i]);
+//console.log("present time " +presentTime);
   }
 
 var today = moment();
@@ -17,10 +17,9 @@ $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 
 var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-var military = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var military = [09, 10, 11, 12, 13, 14, 15, 16, 17];
 var dailyEntry = ["", "", "", "", "", "", "", "", ""];
 var presentTime = (moment().hour());
-//console.log(moment().hour())
 
 
 var rootDiv = $('#root');
@@ -29,17 +28,27 @@ var rootDiv = $('#root');
 
 for (var i = 0; i < hours.length; i++) {
 
+console.log(JSON.stringify([i]));
+
     var baseRow = $("<div class='row'>");
     var timeBox = $("<div class='hour'>");
     var entryBox = $("<div class='description future'>")
     var btnBox = $("<button class='col-md-1 saveBtn'>")
     var actualBtn = $("<i class='far fa-save fa-lg'></i>")
 
-    var inputForm = $("<input>")
+    //var inputForm = $("<input>")
+    //$(inputForm).css("width","100%")
+
+    //var inputForm = $("<textarea>")
+    //$(inputForm).css("width","100%")
+
+    var inputForm = $("<textarea id='myTextarea[i]'>")
     $(inputForm).css("width","100%")
+    
+    //console.log("Form:" +inputForm)
 
 
-    timeBox.text(hours[i]+"  /  "+military[i]);
+    timeBox.text(hours[i]);
     entryBox.text(dailyEntry[i]);
    
     rootDiv.append(baseRow);
@@ -51,7 +60,27 @@ for (var i = 0; i < hours.length; i++) {
 
   }
 
+/*
+function saveToLocalStorage() {
+  localStorage.setItem('textinput', inputForm.textarea)
 
+}
+*/
+
+function mySave() {
+  var myContent = document.getElementById("myTextarea[i]").value;
+  localStorage.setItem("myContent", myContent);
+  console.log("this"+myContent)
+}
+
+
+
+
+
+
+//$(".saveBtn").click(saveToLocalStorage);
+
+$(".saveBtn").click(mySave);
 
 
 
